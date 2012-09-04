491d6aa8bfd72fa1540c4834186ed72ad06b06cc
Steve Hocking <github@stevehocking.co.uk>
2012-09-04 21:24:41 +0100

    Initial commit
    Working (not great) code

---
+++ PoshHttp/GetHttpCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Management.Automation;
+using System.Net;
+
+namespace PoshHttp
+{
+    [Cmdlet(VerbsCommon.Get, "Http", DefaultParameterSetName = "Addresses")]
+    public class GetHttpCommand : Cmdlet
+    {
+        [Parameter(Position = 0, ParameterSetName = "Addresses", Mandatory = true)]
+        [Parameter(Position = 0, ParameterSetName = "Address", Mandatory = true)]
+        [Parameter(Position = 0, ParameterSetName = "Href", Mandatory = true)]
+        [Parameter(Position = 0, ParameterSetName = "Url", Mandatory = true)]
+        public Uri[] Addresses { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            List<string> content = new List<string>();
+
+            foreach (Uri address in Addresses)
+            {
+                try
+                {
+                    WebRequest request = HttpWebRequest.Create(address);
+                    using (WebResponse response = request.GetResponse())
+                    using (Stream stream = response.GetResponseStream())
+                    using (StreamReader streamReader = new StreamReader(stream))
+                    {
+                        content.Add(streamReader.ReadToEnd());
+                    }
+                }
+                catch (Exception exception)
+                {
+                    WriteError(new ErrorRecord(exception, "HttpError", ErrorCategory.ReadError, address));
+                }
+            }
+
+            WriteObject(content, true);
+        }
+    }
+}
